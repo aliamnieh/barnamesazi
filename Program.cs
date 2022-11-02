2a5508b08283e934698a4a6627561ee216e0ccf0
aliamnieh <117279015+aliamnieh@users.noreply.github.com>
2022-11-02 05:34:47 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello user!");
+            System.Console.ReadKey();
+        }
+    }
+}

fd10609f5fc862f9ae8a267cfe66d39995766f2f
aliamnieh <117279015+aliamnieh@users.noreply.github.com>
2022-11-02 05:18:48 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool content = true;
+            bool nocontent = false;
+            Console.WriteLine("Text is {0}", content);
+            Console.WriteLine("Text is {0}", nocontent);
+            Console.ReadKey();
+        }
+    }
+}
